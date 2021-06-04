d780c0b0e36f1f7200673db8c1bd75ce11a09767
Bahia369 <hsbcoding@gmail.com>
2021-06-04 12:03:16 +0100

    updated
    updated

--- src/PeopleList.js
+++ src/PeopleList.js
@@ -19,6 +19,8 @@
   below that removes the last person from the
   list when it is clicked.
 
+  
+
   5. Stretch goal 2: Add an input next to
   the remove name button and change how it
   works so that it removes the person with
@@ -31,11 +33,25 @@ import React, { useState } from "react";
 const PeopleList = () => {
   const [people, setPeople] = useState(["Dave", "Alice", "Bob"]);
   const [newName, setNewName] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  console.log(people);
 
   function addNewPerson() {
     const newPeople = [...people, newName];
     setPeople(newPeople);
   }
+
+  function remove(index) {
+    setPeople(people.slice(index, -1));
+  }
+
+  function handleRemove1(e) {
+    const removePeople = people.filter((item) => item !== newName);
+    setPeople(removePeople);
+  }
+  // function delName() {
+  //   const filName = people.filter((item) => item !== setInputValue(inputValue));
+  // }
 
   return (
     <div>
@@ -54,6 +70,11 @@ const PeopleList = () => {
           />
         </label>
         <button onClick={addNewPerson}>Add person</button>
+        <button onClick={remove}>Remove Last Person</button>
+        <input
+          type="text"
+          onChange={(event) => setInputValue(event.target.value)}
+        />
       </div>
     </div>
   );
